185c270ab54c5c9f20bb5e37a47e71b56c07f56e
Clevince <30797147+Clevince@users.noreply.github.com>
2019-09-02 18:59:39 +0800

    Inclass Assignment 1

---
+++ Inclass Assignment 1(2).py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+num = int(input("Enter a number: "))
+
+mod = num % 2
+
+if mod > 0:
+
+    print("You picked an odd number.")
+
+else:
+
+    print("You picked an even number.")
+
+
+# In[3]:
+
+
+num = int(input("give me a number to check: "))
+
+check = int(input("give me a number to divide by: "))
+
+
+
+if num % 4 == 0:
+
+    print(num, "is a multiple of 4")
+
+elif num % 2 == 0:
+
+    print(num, "is an even number")
+
+else:
+
+    print(num, "is an odd number")
+
+
+
+if num % check == 0:
+
+    print(num, "divides evenly by", check)
+
+else:
+
+    print(num, "does not divide evenly by", check)
+
